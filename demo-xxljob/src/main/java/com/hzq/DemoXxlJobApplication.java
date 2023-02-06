45b2846c31e1e1a72320e0c21719fcab933dd933
hzq <1014571190@qq.com>
2023-02-06 14:11:01 +0800

    demo-xxljob 初步完成

---
+++ demo-xxljob/src/main/java/com/hzq/DemoXxlJobApplication.java
@@ -0,0 +1,18 @@
+package com.hzq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author hzq
+ * @ClassName com.hzq.DemoXxlJobApplication
+ * @Path PACKAGE_NAME.com.hzq.DemoXxlJobApplication
+ * @Date 2023/2/5 19:53
+ * @Description
+ */
+@SpringBootApplication
+public class DemoXxlJobApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoXxlJobApplication.class, args);
+    }
+}
